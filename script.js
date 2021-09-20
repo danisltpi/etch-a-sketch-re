d4cd12a274221d95fd3d3eba7ac8ddf907531ce3
thien <lcdns@protonmail.com>
2021-09-20 15:12:18 +0200

    added rgb

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-// create 16x16 grid of squares
+function getRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0;i < 6; i++) { 
+    color += letters[Math.floor(Math.random() * 16)];
+  } 
+  return color;
+}
+
 function createCanvas(squares) {
   while (squares > 100) {
     squares = prompt("Too many! Input less squares!");
   }
-  // set square size
-
-  let numberOfSquares = squares ** 2;
+  const numberOfSquares = squares ** 2;
   const canvas = document.querySelector(".canvas");
+  // calculate square size
+  const size = 600 / squares;
   // remove already existing squares
   while (canvas.firstChild) {
     canvas.removeChild(canvas.firstChild);  
@@ -15,16 +23,17 @@ function createCanvas(squares) {
   for (let i = 0; i < numberOfSquares; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.style.height = `${size}px`;
+    square.style.width = `${size}px`;
     canvas.appendChild(square);
   }
 }
 
 function changeColor(event) {
-  event.target.style.backgroundColor = "gray";
+  event.target.style.backgroundColor = getRandomColor();
 }
 
 function resetCanvas(event) {
-  console.log("Canvas is being reset");
   let input = prompt("How many squares do you want on each side?");
   createCanvas(input);
 
